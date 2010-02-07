140407e82e531f2eb75629dca192f0ec05217460
niunio <niunio@ea66c5fd-f767-4db5-9ac0-88130696a7d4>
2010-02-07 18:29:36 +0000

    * Documentation prepared for next release. * Picture in "About" dialog fixed. * IDE main menu accelerators fixed. * Now hte IDE marks source as "Not modified" when saved from menu option "Save   as...".

--- src/ide/UnitMainWindow.pas
+++ src/ide/UnitMainWindow.pas
@@ -1,5 +1,28 @@
 (* The main window of the application. *)
 UNIT UnitMainWindow;
+(*
+  Copyright (c) 2009, 2010 Guillermo Martínez J.
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+*)
+
 
 {$mode objfpc}{$H+}
 
@@ -73,7 +96,7 @@ USES
 
 
 CONST
-  TITLE_BASE = 'Z80-Pascal IDE 0.2.0';
+  TITLE_BASE = 'Z80-Pascal IDE 0.1.0';
 { Status bar panels. }
   MODIFIED_PANEL = 0;
 
@@ -220,6 +243,7 @@ CONST
   PROCEDURE TMainWindow.FileSaveAsAccept(Sender: TObject);
   BEGIN
     SynEdit.Lines.SaveToFile (SELF.FileSaveAs.Dialog.FileName);
+    SynEdit.Modified := FALSE;
     fFileName := SELF.FileSaveAs.Dialog.FileName;
     UpdateStatus;
   END;
